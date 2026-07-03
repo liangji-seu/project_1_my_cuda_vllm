dbc9703f0cb7fd3484960a2977c8ce81dd1b18c8
liangji <15262272286@163.com>
2026-07-03 18:33:45 +0800

    feat: support Buffer layer

--- sys/include/base/Buffer.h
+++ sys/include/base/Buffer.h
@@ -12,7 +12,7 @@ class Buffer : public NoCopyable {
 private:
     void* ptr                                          = nullptr;             // 内存地址
     size_t byte_size                                   = 0;                   // 内存大小
-    DeviceType_t buffer_device_type                    = DeviceType_t::Unknown; // 内存的设备类型
+    DeviceType_t device_type                           = DeviceType_t::Unknown; // 内存的设备类型
     std::shared_ptr<DeviceController> controller;                             // 设备控制器
     bool flag_is_external                              = false;                // true:外部指定内存  false:控制器开辟内存
 
@@ -20,20 +20,21 @@ public:
     explicit Buffer() = default;
     ~Buffer();
 
-    explicit Buffer(void* ptr                                              = nullptr,
-                    size_t byte_size                                       = 0,
+    explicit Buffer( size_t byte_size,
+                    void* ptr                                              = nullptr,
                     DeviceType_t buffer_device_type                        = DeviceType_t::Unknown,
                     std::shared_ptr<DeviceController> controller           = nullptr,
-                    bool flag_is_external                                  = false);
+                    bool flag_is_external                                  = false
+                                                          );
 
     /*操作方法*/
-    void buffer_copy_from(const Buffer* buffer) const;                          // 从目标buffer内存，拷贝固定byte_size长度的内存到本buffer
-    void buffer_self_allocate() const;                                          // 开始自行调用控制器开辟内存
+    void buffer_copy_from(const Buffer& buffer);                          // 从目标buffer内存，拷贝固定byte_size长度的内存到本buffer
+    void buffer_self_allocate() ;                                          // 开始自行调用控制器开辟内存
 
     /*查询方法*/
     size_t get_byte_size() const;                                               // 获取内存大小
     void* get_ptr() const;                                                      // 获取内存地址
-    DeviceType_t get_buffer_device_type() const;                                // 获取内存的设备类型
+    DeviceType_t get_device_type() const;                                // 获取内存的设备类型
     std::shared_ptr<DeviceController> get_controller() const;                   // 获取设备控制器
     bool get_flag_is_external() const;                                          // 获取外部内存标志
 };
--- sys/src/base/Buffer.cpp
+++ sys/src/base/Buffer.cpp
@@ -2,52 +2,84 @@
 
 namespace base {
 
-Buffer::~Buffer() {
-    // TODO
-}
-
-Buffer::Buffer(void* ptr,
-               size_t byte_size,
+//buffer创建对象，默认必须是内部自行申请内存。且立刻当场分配
+Buffer::Buffer(size_t byte_size,
+    void* ptr,
+               
                DeviceType_t buffer_device_type,
                std::shared_ptr<DeviceController> controller,
-               bool flag_is_external)
+               bool flag_is_external
+               )
     : ptr(ptr),
       byte_size(byte_size),
-      buffer_device_type(buffer_device_type),
       controller(controller),
-      flag_is_external(flag_is_external) {}
-
-void Buffer::buffer_copy_from(const Buffer* buffer) const {
-    // TODO
+      flag_is_external(flag_is_external) {
+    if(!ptr && controller){
+        device_type = controller->get_device_type();
+        flag_is_external = false;
+        ptr = controller->mem_alloc(byte_size);
+    }
 }
 
-void Buffer::buffer_self_allocate() const {
-    // TODO
+
+Buffer::~Buffer() {
+    if(!flag_is_external){
+        //自行分配的内存，当buffer结束，需要随着对象一起释放
+        if(ptr && controller){
+            //这个buffer里面有内存地址，且有设备控制器用于释放
+            controller->mem_release(ptr);
+            ptr = nullptr;
+        }
+    }
+    //外部的，别的buffer的内存，不需要随着这个buffer结束而释放
+}
+
+
+//从另一个buffer里面拷贝内存过来
+void Buffer::buffer_copy_from(const Buffer& buffer) {
+    CHECK(controller != nullptr);
+    CHECK(buffer.get_ptr() != nullptr);
+
+    //只拷贝少数的那一个
+    size_t copy_size = byte_size < (buffer.get_byte_size()) ? (byte_size):(buffer.get_byte_size());
+    CHECK(device_type != DeviceType_t::Unknown);
+    CHECK(buffer.get_device_type() != DeviceType_t::Unknown);
+    byte_size = copy_size;
+    return controller->mem_copy(buffer.get_ptr(), ptr, copy_size,
+                                buffer.get_device_type(), device_type);
+
+}
+
+//重新分配新的byte_size个内存
+void Buffer::buffer_self_allocate() {
+    CHECK(controller != nullptr);
+    CHECK(byte_size > 0);
+    CHECK(flag_is_external == false);
+    if(ptr){
+        controller->mem_release(ptr);
+        ptr = controller->mem_alloc(byte_size);
+    }
+    return;
 }
 
 size_t Buffer::get_byte_size() const {
-    // TODO
-    return 0;
+    return byte_size;
 }
 
 void* Buffer::get_ptr() const {
-    // TODO
-    return nullptr;
+    return ptr;
 }
 
-DeviceType_t Buffer::get_buffer_device_type() const {
-    // TODO
-    return DeviceType_t::Unknown;
+DeviceType_t Buffer::get_device_type() const {
+    return device_type;
 }
 
 std::shared_ptr<DeviceController> Buffer::get_controller() const {
-    // TODO
-    return nullptr;
+    return controller;
 }
 
 bool Buffer::get_flag_is_external() const {
-    // TODO
-    return false;
+    return flag_is_external;
 }
 
 } // namespace base
